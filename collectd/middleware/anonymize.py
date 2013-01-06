cc4692a3bd6dbdbb25a199d116c3b78cb2ebd884
Michael Bauer <mihi@lo-res.org>
2013-01-06 12:14:58 +0100

    performance increase for the anonymize middleware - cryptopan is initialized on load

--- collectd/middleware/anonymize.py
+++ collectd/middleware/anonymize.py
@@ -11,10 +11,10 @@ except ImportError:
   import random
   anonymizeseed="".join(chr(x) for x in random.sample(range(0,256),32))
 import re
+cp=CryptoPan(anonymizeseed)
 
 def process(message):
   ips=re.findall("([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})",message)
-  cp=CryptoPan(anonymizeseed)
   ips=((ip,cp.anonymize(ip)) for ip in ips)
   return reduce(lambda x,y: re.sub(y[0],y[1],x), ips,message)
 
